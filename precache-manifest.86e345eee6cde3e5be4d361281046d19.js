a8f42db6b49700686ca55c90f2b15c636e5aa13b
natalie9 <natalietolentino9@gmail.com>
2020-08-11 14:34:09 -0300

    Updates

---
+++ precache-manifest.86e345eee6cde3e5be4d361281046d19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6aa8c47cca0ac0967f64bac26b9b597",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2e6a3cfd9c8b71e7dd7",
+    "url": "/static/css/main.596a684a.chunk.css"
+  },
+  {
+    "revision": "dbe5d88369242ea3b724",
+    "url": "/static/js/2.37329a9d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.37329a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e6a3cfd9c8b71e7dd7",
+    "url": "/static/js/main.efa929cd.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "710c395a3a7f3a3bde20844f481ddf9e",
+    "url": "/static/media/Logo_IPElab.710c395a.png"
+  },
+  {
+    "revision": "243fcdd64a2fa20fdc8edaac62a3baeb",
+    "url": "/static/media/logo.243fcdd6.png"
+  }
+]);
